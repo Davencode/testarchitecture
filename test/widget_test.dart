f3c3f3977088ba2ded320d16d287adfb78ebd85f
Davide Belardi <davidebelardi1994@gmail.com>
2023-10-09 16:09:29 +0200

    Second commit and push

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:testarchitecture/main.dart';
+import 'package:testarchitecture/presentationLayer/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
