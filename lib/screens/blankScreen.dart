f90a236d5bd5a8142bdc5668574f0f4b21cca475
RamonaRuoppo <ramonaruoppo@hotmail.it>
2024-02-02 18:24:05 +0100

    menu FloatingActionButton added on the top leading with no action while pressed. AppBar commented

--- lib/screens/blankScreen.dart
+++ lib/screens/blankScreen.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:testarchitecture/screens/splash_screen.dart';
 
 class BlankScreen extends StatelessWidget {
-
   const BlankScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pagina Blank'),
+      drawer: SplashScreen(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FloatingActionButton(
+          backgroundColor: Colors.white,
+          foregroundColor: Color(0xFF0E5D6B),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18),
+          ),
+          onPressed: () {},
+          hoverColor: Colors.grey,
+          child: const Icon(Icons.menu),
+        ),
       ),
+      /*appBar: AppBar(
+        title: Text('Pagina Blank'),
+      ),*/
       body: Center(
         child: Text('Questa è una pagina vuota.'),
       ),
